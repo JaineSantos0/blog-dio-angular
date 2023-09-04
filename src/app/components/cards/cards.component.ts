ddcedcc3abfba95bd227593a713d9617152440db
Jaine Santos <jainejosiane@gmail.com>
2023-09-04 00:03:08 -0300

    feat: creating dynamic components

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.css']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit{
+
+  @Input()
+  photoCover : string = "";
+  @Input()
+  cardTitle : string = "";
+  @Input()
+  cardContent : string = "";
+
+  constructor() {}
+
+  ngOnInit(): void {
+  }
 
 }
